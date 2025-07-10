cf69b5b354ea684a97e1d00b54c7ce4abdc09f55
James Lee <aardvarkpepper@gmail.com>
2025-07-09 20:22:57 -0400

    Fix sort by price or name module.

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import { Product } from './models/Product.ts';
 import { PhysicalProduct } from './models/PhysicalProduct.ts';
 import { DigitalProduct } from './models/DigitalProduct.ts';
 import { calculateTax } from './utils/taxCalculator.ts';
+import { sortByPriceOrName } from './utils/sort.ts';
 
-const hamsterPellets = new PhysicalProduct("skuHamsterPellets", "Hamster Pellets", 1.23, 0.5);
-const digitalHamsterPellets = new DigitalProduct("skyDigitalHamsterPellets", "Digital Hamster Pellets", 0.10, 0.01);
+const apple = new PhysicalProduct("skuApple", "Apple", 3, 0.5);
+const banana = new PhysicalProduct("skuBanana", "Banana", 2, 0.4);
+const chupacabra = new DigitalProduct("skuChupacabra", "Chupacabra", -1, 0.3);
 
 const productArray: Array<Product> = [];
-productArray.push(hamsterPellets);
-productArray.push(digitalHamsterPellets);
+productArray.push(apple);
+productArray.push(banana);
+productArray.push(chupacabra);
 
 console.log("=========");
 for (let item of productArray) {
@@ -16,3 +19,6 @@ for (let item of productArray) {
   console.log(`Has a final price of $${calculateTax(item)}`);
   console.log("=========");
 }
+
+console.log(sortByPriceOrName(productArray, "name"));
+console.log(sortByPriceOrName(productArray, "price"));
--- src/utils/sort.ts
+++ src/utils/sort.ts
@@ -1,16 +1,23 @@
 import { Product } from '../models/Product.ts';
 
-// const sortByPriceOrName = (productArray: Array<Product>, priceOrName: "price" | "name"): Array<Product> => {
-//   const sortedArray: Array<Product> = [];
-//   for (let i = 0; i < productArray.length; i++) {
-//     productArray[i][priceOrName]
-//   }
-// }
+export const sortByPriceOrName = (productArray: Array<Product>, priceOrName: "price" | "name"): Array<Product> => {
+  const sortedArray: Array<Product> = [];
+  for (let i = 0; i < productArray.length; i++) {
+    const sortedIndex = findIndexOf(sortedArray, priceOrName, productArray[i][priceOrName]);
+    console.log(`Inserting at index ${sortedIndex}, ${productArray[i].name}`);
+    if (sortedIndex === -1) {
+      sortedArray.push(productArray[i])
+    } else {
+      sortedArray.splice(sortedIndex, 0, productArray[i]);
+    }
+  }
+  return sortedArray;
+}
 
 const findIndexOf = (sortedArray: Array<Product>, priceOrName: "price" | "name", searchTerm: number | string): number => {
   let start = 0, end = sortedArray.length - 1;
   let mid;
-  while (start < end) {
+  while (start <= end) {
     mid = Math.floor((start + end) / 2);
     if (sortedArray[mid][priceOrName] === searchTerm) {
       return mid;
@@ -20,29 +27,30 @@ const findIndexOf = (sortedArray: Array<Product>, priceOrName: "price" | "name",
       end = mid - 1;
     }
   }
-  return -1;
+  //return -1;
+  return start;
 }
 
-const findIndex = (searchTerm: number = 4, sortedArray: Array<number> = [0, 1, 2, 4, 5, 6, 8]): number => {
-  let start = 0, end = sortedArray.length - 1;
-  let mid;
-  while (start <= end) {
-    mid = Math.floor((start + end) / 2);
-    //console.log(`fI, ${start}, ${mid}, ${end}`);
-    if (sortedArray[mid] === searchTerm) {
-      return mid;
-    } else if (sortedArray[mid] < searchTerm) {
-      start = mid + 1;
-    } else if (sortedArray[mid] > searchTerm) {
-      end = mid - 1;
-    }
-  }
-  return -1;
-}
+// const findIndex = (searchTerm: number = 4, sortedArray: Array<number> = [0, 1, 2, 4, 5, 6, 8]): number => {
+//   let start = 0, end = sortedArray.length - 1;
+//   let mid;
+//   while (start <= end) {
+//     mid = Math.floor((start + end) / 2);
+//     console.log(`fI, ${start}, ${mid}, ${end}`);
+//     if (sortedArray[mid] === searchTerm) {
+//       return mid;
+//     } else if (sortedArray[mid] < searchTerm) {
+//       start = mid + 1;
+//     } else if (sortedArray[mid] > searchTerm) {
+//       end = mid - 1;
+//     }
+//   }
+//   return end;
+// }
 
 // console.log(findIndex(3)); // -1
 // console.log(findIndex(4)); //  3
 // console.log(findIndex(0)); // 0
 // console.log(findIndex(8)); // 6
 // console.log(findIndex(9)); // -1
-// console.log(findIndex(-1)); // -1
+//console.log(findIndex(-1)); // -1
